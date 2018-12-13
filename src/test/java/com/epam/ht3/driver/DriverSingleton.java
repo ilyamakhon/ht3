8114e43aaa5c9a3299831eb21beffd7df737b2cb
ilyamakhon <i.makhon@yandex.ru>
2018-12-13 23:27:42 +0300

    geckodriver timeouts fixes

--- src/test/java/com/epam/ht3/driver/DriverSingleton.java
+++ src/test/java/com/epam/ht3/driver/DriverSingleton.java
@@ -15,17 +15,17 @@ public class DriverSingleton {
     private static WebDriver driver;
     private static final Logger logger = LogManager.getRootLogger();
     private static final String WEBDRIVER_GECKO_DRIVER = "webdriver.gecko.driver";
-    private static final String GECKODRIVER_GECKODRIVER_EXE_PATH = ".\\geckodriver\\geckodriver.exe";
+    private static final String GECKODRIVER_EXE_PATH = "src\\geckodriver\\geckodriver.exe";
 
     private DriverSingleton(){};
 
 
     public static WebDriver getDriver(){
         if (null == driver){
-            System.setProperty(WEBDRIVER_GECKO_DRIVER, GECKODRIVER_GECKODRIVER_EXE_PATH);
+            System.setProperty(WEBDRIVER_GECKO_DRIVER, GECKODRIVER_EXE_PATH);
             driver = new FirefoxDriver();
-            driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+            driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
             driver.manage().window().maximize();
             logger.info("Browser started");
         }
