4ce0bd2dc65898f5d2400867f5b01cdb504c3f7e
ilyamakhon <i.makhon@yandex.ru>
2018-12-13 23:33:13 +0300

    repository deletion test added

--- src/test/java/com/epam/ht3/GitHubAutomationTest.java
+++ src/test/java/com/epam/ht3/GitHubAutomationTest.java
@@ -14,6 +14,8 @@ public class GitHubAutomationTest
 	private final String PASSWORD = "Time4Death!";
 	private final int REPOSITORY_NAME_POSTFIX_LENGTH = 6;
 
+	private String REPOSITORY_NAME;
+
 	@BeforeMethod(description = "Init browser")
 	public void setUp()
 	{
@@ -21,20 +23,34 @@ public class GitHubAutomationTest
 		steps.openBrowser();
 	}
 
-	@Test
+	@Test(priority = 2)
 	public void oneCanCreateProject()
 	{
 		steps.loginGithub(USERNAME, PASSWORD);
 		String repositoryName = steps.generateRandomRepositoryNameWithCharLength(REPOSITORY_NAME_POSTFIX_LENGTH);
 		steps.createNewRepository(repositoryName, "auto-generated test repo");
 		Assert.assertEquals(steps.getCurrentRepositoryName(), repositoryName);
+
+		REPOSITORY_NAME = steps.getCurrentRepositoryName();
 	}
 
-	@Test(description = "Login to Github")
+	@Test(description = "Login to Github", priority = 1)
 	public void oneCanLoginGithub()
 	{
 		steps.loginGithub(USERNAME, PASSWORD);
 		Assert.assertEquals(USERNAME, steps.getLoggedInUserName());
+	}
+
+	@Test(priority = 3)
+	public void deleteRepository() {
+		//Find repository
+		steps.loginGithub(USERNAME, PASSWORD);
+		boolean repositoryFound = steps.findRepository(this.REPOSITORY_NAME);
+		Assert.assertTrue(repositoryFound, "Repository " + this.REPOSITORY_NAME + " not found");
+
+		//Delete repository
+		boolean isRepositoryDeleted = steps.deleteRepository(this.REPOSITORY_NAME);
+		Assert.assertTrue(isRepositoryDeleted, "Repository " + this.REPOSITORY_NAME + " wasn't deleted");
 	}
 
 	@AfterMethod(description = "Stop Browser")
