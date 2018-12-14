86a24b7d3beaedd1d0afe140037755829bf1c4ef
ilyamakhon <i.makhon@yandex.ru>
2018-12-14 18:34:23 +0300

    steps added

--- src/test/java/com/epam/ht3/steps/Steps.java
+++ src/test/java/com/epam/ht3/steps/Steps.java
@@ -11,79 +11,84 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 
-public class Steps
-{
-	private WebDriver driver;
+public class Steps {
+    private final Logger logger = LogManager.getRootLogger();
+    private WebDriver driver;
 
-	private final Logger logger = LogManager.getRootLogger();
+    public void openBrowser() {
+        driver = DriverSingleton.getDriver();
+    }
 
-	public void openBrowser()
-	{
-		driver = DriverSingleton.getDriver();
-	}
+    public void closeBrowser() {
+        DriverSingleton.closeDriver();
+    }
 
-	public void closeBrowser()
-	{
-		DriverSingleton.closeDriver();
-	}
+    public void loginGithub(String username, String password) {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.openPage();
+        loginPage.login(username, password);
+    }
 
-	public void loginGithub(String username, String password)
-	{
-		LoginPage loginPage = new LoginPage(driver);
-		loginPage.openPage();
-		loginPage.login(username, password);
-	}
+    public String getLoggedInUserName() {
+        LoginPage loginPage = new LoginPage(driver);
+        return loginPage.getLoggedInUserName().trim().toLowerCase();
+    }
 
-	public String getLoggedInUserName()
-	{
-		LoginPage loginPage = new LoginPage(driver);
-		return loginPage.getLoggedInUserName().trim().toLowerCase();
-	}
+    public void createNewRepository(String repositoryName, String repositoryDescription) {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.clickOnCreateNewRepositoryButton();
+        CreateNewRepositoryPage createNewRepositoryPage = new CreateNewRepositoryPage(driver);
+        createNewRepositoryPage.createNewRepository(repositoryName, repositoryDescription);
+    }
 
-	public void createNewRepository(String repositoryName, String repositoryDescription)
-	{
-		MainPage mainPage = new MainPage(driver);
-		mainPage.clickOnCreateNewRepositoryButton();
-		CreateNewRepositoryPage createNewRepositoryPage = new CreateNewRepositoryPage(driver);
-		createNewRepositoryPage.createNewRepository(repositoryName, repositoryDescription);
-	}
+    public boolean currentRepositoryIsEmpty() {
+        CreateNewRepositoryPage createNewRepositoryPage = new CreateNewRepositoryPage(driver);
+        return createNewRepositoryPage.isCurrentRepositoryEmpty();
+    }
 
-	public boolean currentRepositoryIsEmpty()
-	{
-		CreateNewRepositoryPage createNewRepositoryPage = new CreateNewRepositoryPage(driver);
-		return createNewRepositoryPage.isCurrentRepositoryEmpty();
-	}
+    public String getCurrentRepositoryName() {
+        CreateNewRepositoryPage createNewRepositoryPage = new CreateNewRepositoryPage(driver);
+        return createNewRepositoryPage.getCurrentRepositoryName();
+    }
 
-	public String getCurrentRepositoryName(){
-		CreateNewRepositoryPage createNewRepositoryPage = new CreateNewRepositoryPage(driver);
-		return createNewRepositoryPage.getCurrentRepositoryName();
-	}
+    public String generateRandomRepositoryNameWithCharLength(int howManyChars) {
+        String repositoryNamePrefix = "testRepo_";
+        return repositoryNamePrefix.concat(Utils.getRandomString(howManyChars));
+    }
 
-	public String generateRandomRepositoryNameWithCharLength(int howManyChars){
-		String repositoryNamePrefix = "testRepo_";
-		return repositoryNamePrefix.concat(Utils.getRandomString(howManyChars));
-	}
+    public boolean findRepository(String repositoryName, String searchingType) {
+        return findRepositoryBySearchingType(repositoryName, searchingType);
+    }
 
-	public boolean findRepository(String repositoryName) {
-		MainPage mainPage = new MainPage(driver);
-		mainPage.clickToMyRepositories();
+    public boolean deleteRepository(String repositoryName) {
+        RepositoryPage repositoryPage = new RepositoryPage(driver);
+        repositoryPage.deleteRepository(repositoryName);
 
-		MyRepositoriesPage myRepositoriesPage = new MyRepositoriesPage(driver);
-		do {
-			if (myRepositoriesPage.findRepository(repositoryName)) {
-				myRepositoriesPage.goToRepositoryPage(repositoryName);
-				return true;
-			}
-		} while (!myRepositoriesPage.findRepository(repositoryName));
-		return false;
-	}
+        MainPage mainPage = new MainPage(driver);
+        return mainPage.checkRepositoryDeletion(repositoryName);
+    }
 
-	public boolean deleteRepository(String repositoryName) {
-		RepositoryPage repositoryPage = new RepositoryPage(driver);
-		repositoryPage.deleteRepository(repositoryName);
+    public boolean findRepositoryBySearchingType(String repositoryName, String searchingType) {
+        MyRepositoriesPage myRepositoriesPage = new MyRepositoriesPage(driver);
+        MainPage mainPage = new MainPage(driver);
 
-		MainPage mainPage = new MainPage(driver);
-		return mainPage.checkRepositoryDeletion(repositoryName);
-	}
+        switch (searchingType) {
+            case "repositoriesPage":
+                mainPage.clickToMyRepositories();
+                do {
+                    if (myRepositoriesPage.findRepository(repositoryName)) {
+                        return true;
+                    }
+                } while (!myRepositoriesPage.findRepository(repositoryName));
+
+            case "searchingSidebar":
+                return mainPage.findRepositoryViaSearchingSidebar(repositoryName);
+
+            case "headerNavSearchInput":
+                return mainPage.findRepositoryViaHeaderSearchInput(repositoryName);
+
+        }
+        return false;
+    }
 
 }
