87949ec3a0eb1cc91925ace15ddf051ac8159da3
ilyamakhon <i.makhon@yandex.ru>
2018-12-14 21:09:07 +0300

    comment added

--- src/test/java/com/epam/ht3/dataprovider/DataProviderClass.java
+++ src/test/java/com/epam/ht3/dataprovider/DataProviderClass.java
@@ -11,9 +11,10 @@ public class DataProviderClass {
         switch (method.getName()) {
             case "tst_repositorySearch_Via":
                 return new Object[][]{
-                        {"headerNavSearchInput"},
+                        { "repositoriesPage" },
                         { "searchingSidebar" },
-                        { "repositoriesPage" }
+                        {"headerNavSearchInput"}//github bug??? github does not find repository using this type of search
+
                 };
             case "tst_repositoryDeletion":
                 return new Object[][]{
