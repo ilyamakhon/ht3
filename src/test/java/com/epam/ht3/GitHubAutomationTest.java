a9efddc55f0de37ccca7c4e28921d469d15f5fec
ilyamakhon <i.makhon@yandex.ru>
2018-12-14 18:36:27 +0300

    tst_repositorySearchVia added tst_repositoryDeletion parametrization added

--- src/test/java/com/epam/ht3/GitHubAutomationTest.java
+++ src/test/java/com/epam/ht3/GitHubAutomationTest.java
@@ -1,62 +1,67 @@
 package com.epam.ht3;
 
+import com.epam.ht3.dataprovider.DataProviderClass;
+import com.epam.ht3.steps.Steps;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import com.epam.ht3.steps.Steps;
+public class GitHubAutomationTest {
+    private final String USERNAME = "testautomationuser";
+    private final String PASSWORD = "Time4Death!";
 
-public class GitHubAutomationTest
-{
-	private Steps steps;
-	private final String USERNAME = "testautomationuser";
-	private final String PASSWORD = "Time4Death!";
-	private final int REPOSITORY_NAME_POSTFIX_LENGTH = 6;
+    private final int REPOSITORY_NAME_POSTFIX_LENGTH = 6;
+    private Steps steps;
+    private String REPOSITORY_NAME;
+    private boolean isRepositoryFound;
 
-	private String REPOSITORY_NAME;
+    @BeforeMethod(description = "Init browser")
+    public void setUp() {
+        steps = new Steps();
+        steps.openBrowser();
+    }
 
-	@BeforeMethod(description = "Init browser")
-	public void setUp()
-	{
-		steps = new Steps();
-		steps.openBrowser();
-	}
+    @Test()
+    public void oneCanCreateProject() {
+        steps.loginGithub(USERNAME, PASSWORD);
+        String repositoryName = steps.generateRandomRepositoryNameWithCharLength(REPOSITORY_NAME_POSTFIX_LENGTH);
+        steps.createNewRepository(repositoryName, "auto-generated test repo");
+        Assert.assertEquals(steps.getCurrentRepositoryName(), repositoryName);
 
-	@Test(priority = 2)
-	public void oneCanCreateProject()
-	{
-		steps.loginGithub(USERNAME, PASSWORD);
-		String repositoryName = steps.generateRandomRepositoryNameWithCharLength(REPOSITORY_NAME_POSTFIX_LENGTH);
-		steps.createNewRepository(repositoryName, "auto-generated test repo");
-		Assert.assertEquals(steps.getCurrentRepositoryName(), repositoryName);
+        REPOSITORY_NAME = steps.getCurrentRepositoryName();
+    }
 
-		REPOSITORY_NAME = steps.getCurrentRepositoryName();
-	}
+    @Test(description = "Login to Github")
+    public void oneCanLoginGithub() {
+        steps.loginGithub(USERNAME, PASSWORD);
+        Assert.assertEquals(USERNAME, steps.getLoggedInUserName());
+    }
 
-	@Test(description = "Login to Github", priority = 1)
-	public void oneCanLoginGithub()
-	{
-		steps.loginGithub(USERNAME, PASSWORD);
-		Assert.assertEquals(USERNAME, steps.getLoggedInUserName());
-	}
+    @Test(dataProvider = "dataProvider", dataProviderClass = DataProviderClass.class)
+    public void tst_repositoryDeletion(String searchingType) {
+        //Find repository
+        steps.loginGithub(USERNAME, PASSWORD);
 
-	@Test(priority = 3)
-	public void deleteRepository() {
-		//Find repository
-		steps.loginGithub(USERNAME, PASSWORD);
-		boolean isRepositoryFound = steps.findRepository(this.REPOSITORY_NAME);
-		Assert.assertTrue(isRepositoryFound, "Repository " + this.REPOSITORY_NAME + " not found");
+        isRepositoryFound = steps.findRepository(this.REPOSITORY_NAME, searchingType);
+        Assert.assertTrue(isRepositoryFound, "Repository " + this.REPOSITORY_NAME + " not found");
 
-		//Delete repository
-		boolean isRepositoryDeleted = steps.deleteRepository(this.REPOSITORY_NAME);
-		Assert.assertTrue(isRepositoryDeleted, "Repository " + this.REPOSITORY_NAME + " wasn't deleted");
-	}
+        //Delete repository
+        boolean isRepositoryDeleted = steps.deleteRepository(this.REPOSITORY_NAME);
+        Assert.assertTrue(isRepositoryDeleted, "Repository " + this.REPOSITORY_NAME + " wasn't deleted");
+    }
 
-	@AfterMethod(description = "Stop Browser")
-	public void stopBrowser()
-	{
-		steps.closeBrowser();
-	}
+    @Test(dataProvider = "dataProvider", dataProviderClass = DataProviderClass.class)
+    public void tst_repositorySearch_Via(String searchingType) {
+        steps.loginGithub(USERNAME, PASSWORD);
+
+        isRepositoryFound = steps.findRepository(this.REPOSITORY_NAME, searchingType);
+        Assert.assertTrue(isRepositoryFound, "Repository " + this.REPOSITORY_NAME + " not found");
+    }
+
+    @AfterMethod(description = "Stop Browser")
+    public void stopBrowser() {
+        steps.closeBrowser();
+    }
 
 }
